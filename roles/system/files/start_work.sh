7d46b066b7c87032c3fcd904110505c7bd650c66
danieljamesrees <d_j_rees@hotmail.com>
2017-12-02 15:38:09 +0000

    Refactor local workstation-specific change.

--- roles/system/files/start_work.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-gnome-terminal --working-directory=Projects/personal_ubuntu_setup/
-gnome-terminal --working-directory=Projects/bringit.bitbucket_io/
-gnome-terminal --working-directory=Projects/bringit/
-
-firefox &
