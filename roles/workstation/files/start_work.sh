00d1ec73e98b888cb9b538c4029f6cc33c7dfc80
danieljamesrees <d_j_rees@hotmail.com>
2017-12-20 07:40:54 +0000

    Add fix_wifi playbook and call on startup.

--- roles/workstation/files/start_work.sh
+++ roles/workstation/files/start_work.sh
@@ -6,3 +6,9 @@ gnome-terminal --working-directory=Projects/bringit.bitbucket_io/
 gnome-terminal --working-directory=Projects/bringit/
 
 firefox &
+
+cd Projects/personal_ubuntu_setup
+
+ansible-playbook playbooks/local/fix_wifi.yml
+
+cd ../..
